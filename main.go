0a1afa8f112178726f3c169fd58ba0e473ec8bf2
Lipskiii <max.lipski@hotmail.com>
2024-05-07 19:35:57 +0200

    implemented very basic calculator

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter first number:")
+	var number1, number2 int
+	_, _ = fmt.Scan(&number1)
+	fmt.Println("Enter second number:")
+	_, _ = fmt.Scan(&number2)
+	fmt.Println("Enter desired operator (+-*/%):")
+	operator, _, _ := reader.ReadRune()
+
+	switch op := operator; op {
+	case '+':
+		fmt.Println(add(number1, number2))
+	case '-':
+		fmt.Println(subtract(number1, number2))
+	case '*':
+		fmt.Println(multiply(number1, number2))
+	case '/':
+		fmt.Println(divide(number1, number2))
+	case '%':
+		fmt.Println(modulo(number1, number2))
+	default:
+		fmt.Println("Operator not found, try again.")
+	}
+}
+
+func add(number1 int, number2 int) int {
+	return number1 + number2
+}
+
+func subtract(number1 int, number2 int) int {
+	return number1 - number2
+}
+
+func multiply(number1 int, number2 int) int {
+	return number1 * number2
+}
+
+func divide(number1 int, number2 int) int {
+	return number1 / number2
+}
+
+func modulo(number1 int, number2 int) int {
+	return number1 % number2
 }
